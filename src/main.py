8235e85e8b47d58cdde76132883cd84262c223a3
Sergei Vasilchenko <liketravelin@gmail.com>
2020-05-17 22:27:39 +0300

    init class for geometry figures

---
+++ src/main.py
@@ -0,0 +1,16 @@
+class Figure:
+
+    def __init__(self):
+    pass
+
+class Triangle(Figure):
+    pass
+
+class Rectangle(Figure):
+    pass
+
+class Square(Figure):
+    pass
+
+class Circle(Figure):
+    pass
