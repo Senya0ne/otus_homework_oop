3912ba816809e6b2953e7f9c63b0ecd4196e33dc
Sergei Vasilchenko <liketravelin@gmail.com>
2020-05-18 23:58:41 +0300

    add methods for Triangle

--- code/figure.py
+++ code/figure.py
@@ -1,16 +1,42 @@
 class Figure:
 
-    def __init__(self):
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def get_area(self):
+        return f'Площадь {self.name} равна'
+
+    def get_angles(self, angles):
+        return f'Количество углов у фигуры - ' + angles
+
+    def get_perimeter(self, perimeter):
+        return f'Периметр фигуры равен' + perimeter
+
 
 class Triangle(Figure):
-    pass
 
-class Rectangle(Figure):
-    pass
+    def __init__(self, name):
+        super().__init__(name)
+        self.name = name
 
-class Square(Figure):
-    pass
+    def get_area(self, a, h):
+        super().get_area()
+        area = 0.5 * (a * h)
+        return area
 
-class Circle(Figure):
-    pass
+
+#
+# class Rectangle(Figure):
+#     pass
+#
+# class Square(Figure):
+#     pass
+#
+# class Circle(Figure):
+#     pass
+
+triangle = Triangle('Треугольник')
+print(triangle.name)
+
+a = triangle.get_area(1, 5)
+print(a)
