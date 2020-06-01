a28b13b6f1f20ffb072d21694f16c868125105a2
senya0ne <liketravelin@gmail.com>
2020-06-01 23:33:34 +0300

    add changes for triangle

--- code/Figure.py
+++ code/Figure.py
@@ -1,15 +1,17 @@
 class Figure:
+    angles = None
 
-    def __init__(self, name):
+    def __init__(self, name, angles):
+        self.perimeter = None
+        self.area = None
         self.name = name
+        self.angles = angles
 
     def get_area(self):
-        return f'Площадь {self.name} равна'
+        return f'Площадь {self.name} равна' + self.area
 
     def get_angles(self, angles):
-        return f'Количество углов у фигуры - ' + angles
+        return f'Количество углов у фигуры - ' + self.angles
 
     def get_perimeter(self, perimeter):
-        return f'Периметр фигуры равен' + perimeter
-
-
+        return f'Периметр фигуры равен' + self.perimeter
--- code/Triangle.py
+++ code/Triangle.py
@@ -3,24 +3,23 @@ from code.Figure import Figure
 
 class Triangle(Figure):
 
-    def __init__(self, name):
-        super().__init__(name)
+    def __init__(self, name, angles):
+        super().__init__(name, angles)
 
     def get_area(self, a, h):
-        super().get_area()
-        s = 0.5 *(a*h)
+        s = 0.5 * (a*h)
         return f'Площадь {self.name}а равна ' + str(s)
 
     def get_angles(self, angles):
-        return f'Количество углов равно '
+        return f'Количество углов равно ' + str(self.angles)
 
     def get_perimeter(self, perimeter):
         super().get_perimeter()
         return f'Периметр {self.name}а равна ' + str(p)
 
 
-triangle = Triangle('Треугольник')
-print(triangle.name)
+triangle = Triangle('Треугольник', 3)
+print(triangle.name, triangle.get_angles(3))
 
 a = triangle.get_area(1, 5)
 print(a)
